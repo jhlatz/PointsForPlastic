1a66e10c3309509da73702c7848b919ae314c193
jhlatz <jlatz53@gmail.com>
2018-02-17 09:21:57 -0500

    Login menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,8 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -17,6 +24,15 @@ public class Main extends Application {
 
 	private static Connection con;
 
+	protected Stage stage;
+	protected Scene menu,loginMenu;
+	protected TextField txfSearch, numEntries, txfID, txfPW;
+	protected Label txtInfo;
+	protected VBox root;
+	protected GridPane login;
+	private ComboBox<String> genres;
+	private String UID, UPS;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -25,11 +41,13 @@ public class Main extends Application {
 			System.out.println("Could not connect!");
 		}
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			stage = primaryStage;
+			login = buildLoginPane();
+			loginMenu = new Scene(login);
+			loginMenu.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("Points For Plastic");
+			stage.setScene(loginMenu);
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -37,6 +55,42 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public GridPane buildLoginPane() throws SQLException{
+		GridPane gridLogin = new GridPane();
+		gridLogin.setVgap(5);
+		gridLogin.setHgap(10);
+
+		Label id = new Label("User ID");
+		gridLogin.add(id, 0, 0);
+
+		txfID = new TextField();
+		gridLogin.add(txfID, 0, 1);
+
+		Label pw = new Label("Password");
+		gridLogin.add(pw, 0, 2);
+
+		txfPW = new TextField();
+		gridLogin.add(txfPW, 0, 3);
+
+		Button login = new Button("Login");
+		login.setOnAction(new loginEventHandler());
+		gridLogin.add(login, 0, 4);
+
+		return gridLogin;
+	}
+
+	private class loginEventHandler implements EventHandler<ActionEvent> {
+		@Override
+		public void handle(ActionEvent event) {
+			UID = txfID.getText();
+			UPS = txfPW.getText();
+			//root = buildMenuPane();
+			menu = new Scene(root);
+			menu.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setMaximized(true);
+		}
 	}
 
 	public static void connect() throws SQLException {
